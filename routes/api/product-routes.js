317a9bf632721fed0298e651a693b1544b49ec57
MiaLaFleur <mia.lafleur@yahoo.com>
2022-06-22 17:36:37 -0500

    add model info and Category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      Tag
+    ]
+  })
+    .then(dbProductData => res.status(200).json(dbProductData))
+    .catch(err => res.status(400).json(err))
 });
 
 // get one product
